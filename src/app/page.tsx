95c3d92b91dad93e8733b6b07a5795ca958d0d3f
Rogerio-17 <rogeriojmf10@gmail.com>
2024-06-06 20:34:25 -0300

    Finalizado hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,24 @@
+import { Hero } from '@/components/hero'
 import { Submenu } from '@/components/home/submenu'
 import { getDataHome } from '@/utils/actions/get-data'
 import { HomeProps } from '@/utils/home.type'
+import { Phone } from 'lucide-react'
 
 export default async function Home() {
     const { object }: HomeProps = await getDataHome()
 
-    console.log(object)
+    console.log()
 
     return (
         <section>
             <Submenu />
+            <Hero
+                heading={object.metadata.heading}
+                bannerUrl={object.metadata.banner.url}
+                buttonTitle={object.metadata.cta_button.title}
+                buttonUrl={object.metadata.cta_button.url}
+                icon={<Phone size={20} color="#fff" />}
+            />
         </section>
     )
 }
